6310d0954d9e0f669ee3e6d6c99b1f36544e9028
Ram Patel <rr7patel@gmail.com>
2017-12-05 17:24:54 -0500

    Added some boilerplate

---
+++ sorter_server.c
@@ -0,0 +1,20 @@
+/*THIS IS THE CODE FOR THE SERVER*/
+
+//Imports
+#include "mergesort.c"
+#include "sorter.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <dirent.h>
+#include <sys/stat.h>
+
+int main(int argc, char **(argv)){
+
+    // Make sockets and listen
+
+    return 0;
+}
