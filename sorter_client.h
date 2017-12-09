2c7324953d8dafc8bee35b5a81ca737d7dceeac0
Akshay Mehta <Akshay@Akshays-MacBook-Pro.local>
2017-12-09 01:51:18 -0500

    some clean-up

--- sorter_client.h
+++ sorter_client.h
@@ -19,8 +19,6 @@
 
 /* function prototypes */
 void outputErrorMessage(char *error);
-// void trim(char *token);
-// char *tokenizer(char **input);
 char* attachName(const char* directoryName, const char* name);
 char* attachSorted();
 void outputData();
@@ -30,38 +28,5 @@ int checkRepeat(char *name);
 void* traverseDirectory(void* arg);
 void* sendRequest(void* arg);
 void gotoxy(int x, int y);
-
-// typedef struct node {
-// 	char *color;
-// 	char *director_name;
-// 	char *num_critic_for_reviews;
-// 	char *duration;
-// 	char *director_facebook_likes;
-// 	char *actor_3_facebook_likes;
-// 	char *actor_2_name;
-// 	char *actor_1_facebook_likes;
-// 	char *gross;
-// 	char *genres;
-// 	char *actor_1_name;
-// 	char *movie_title;
-// 	char *num_voted_users;
-// 	char *cast_total_facebook_likes;
-// 	char *actor_3_name;
-// 	char *facenumber_in_poster;
-// 	char *plot_keywords;
-// 	char *movie_imdb_link;
-// 	char *num_user_for_reviews;
-// 	char *language;
-// 	char *country;
-// 	char *content_rating;
-// 	char *budget;
-// 	char *title_year;
-// 	char *actor_2_facebook_likes;
-// 	char *imdb_score;
-// 	char *aspect_ratio;
-// 	char *movie_facebook_likes;
-
-// 	struct node *next;
-// } node;
 
 #endif
